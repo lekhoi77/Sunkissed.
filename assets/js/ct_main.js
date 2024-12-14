ebd6539abc008a13c193b319843fbc6768cf9062
le factuer <lekhoizero@gmail.com>
2024-12-15 00:09:43 +0700

    cmt

--- assets/js/ct_main.js
+++ assets/js/ct_main.js
@@ -180,3 +180,64 @@ scrollToTopButton.addEventListener('click', () => {
     behavior: 'smooth' // Cuộn mượt mà
   });
 });
+  
+// HIỆU ỨNG CON TRỎ CHUỘT
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#ef865e",
+  "#ec805d",
+  "#ef865e",
+  "#ec805d",
+  "#ef865e",
+  "#ec805d",
+  "#ef865e",
+  "#ec805d",
+  "#ef865e",
+  "#ec805d",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+// N/HIỆU ỨNG CON TRỞ CHUỘT
